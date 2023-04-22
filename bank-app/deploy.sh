e62c9d97ea5cdc83b9fecda5e1e5cef18c08636b
K4maS <107948849+K4maS@users.noreply.github.com>
2023-04-22 20:37:05 +0500

    Перенес readme

---
+++ bank-app/deploy.sh
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env sh
+
+# Остановить публикацию при ошибках
+set -e
+
+# Переход в котолог сборки
+cd dist
+
+# Инициация репозитория и загрузка кода в gitHub
+git init
+git add -A
+git commit -m 'deploy'
+
+# f - forse!!!!!!!!!
+git push -f git@github.com:K4maS/coin-practice.git  master:gh-pages
+
+cd -
+
+# https://k4mas.github.io/practic-sit-down-please/
